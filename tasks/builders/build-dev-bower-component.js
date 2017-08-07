0d33aa49d708b2c28b9a1d094b1b613367e68f65
Toni-Jan Keith Monserrat <tjmonsi@gmail.com>
2017-08-07 11:19:08 +0800

    Changed modules to bower_components

--- tasks/builders/build-dev-bower-component.js
+++ tasks/builders/build-dev-bower-component.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp')
 const data = require('../../utils/data')
 const copy = require('../../utils/force-copy')
-const dest = './dist/public/modules'
+const dest = './dist/public/bower_components'
 
 gulp.task('build-dev-bower-components', (done) => {
   copy(gulp.src('./bower_components/**'), {}, dest, done)
