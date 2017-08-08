22fa7f3aa014bf19de459bd0009c583d918f140b
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-08-08 17:06:51 +0800

    merged conflicts
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- utils/build.js
+++ utils/build.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const fs = require('fs')
 
 var config = {}
@@ -24,18 +23,3 @@ module.exports = {
 
 
 
-=======
-const fs = require('fs')
-
-const build = {
-  buildDest: './dist/public',
-  build: 'dev',
-  config: JSON.parse(fs.readFileSync('./src/config/dev.json', 'utf8'))
-}
-
-if (build.config.theme.src.indexOf('modules') === 0) {
-  build.theme = JSON.parse(fs.readFileSync('./src/' + build.config.theme.src + '/theme.json', 'utf8'))
-}
-
-module.exports = build
->>>>>>> Stashed changes
